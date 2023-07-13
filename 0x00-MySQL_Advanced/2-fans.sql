317852f50e5122705811b73687a553a5d68cebd6
Buomkuoth Yian <buom.nyajal@gmail.com>
2023-07-13 17:24:28 +0000

    import table from metal_bands

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,6 +1,4 @@
-/*
- Ranks country origins of bands by the number of (non-unique) fans.
-*/
+-- Ranks country origins of bands by the number of (non-unique) fans.
 
 SELECT origin, SUM(nb_fans) AS nb_fans
 FROM metal_bands
