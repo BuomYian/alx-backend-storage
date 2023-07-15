f0ca5371fa4f7b4b95cf34a5737223c78eedd663
Buomkuoth Yian <buom.nyajal@gmail.com>
2023-07-15 14:18:45 +0000

    split

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,5 @@
--- Retrieves the bands with Glem rock as their main style, ranked by their longetivity
-
-SELECT band_name, (2022 - formed - split) AS lifespan
+-- Retrieves the bands with Glam rock as their main style, ranked by their longevity
+SELECT band_name, (2022 - formed) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
