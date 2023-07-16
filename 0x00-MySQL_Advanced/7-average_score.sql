67ad69de418dea29b8bd31f5067ab56828827ef3
Buomkuoth Yian <buom.nyajal@gmail.com>
2023-07-16 19:35:00 +0000

    fixed

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -4,17 +4,17 @@ CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
     DECLARE total_score FLOAT;
     DECLARE total_count INT;
-
+    
     -- Calculate the total score
     SELECT SUM(score) INTO total_score
     FROM corrections
     WHERE user_id = user_id;
-
+    
     -- Calculate the total count
     SELECT COUNT(*) INTO total_count
     FROM corrections
     WHERE user_id = user_id;
-
+    
     -- Calculate the average score
     UPDATE users
     SET average_score = total_score / total_count
