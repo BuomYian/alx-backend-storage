a494eec71ccfad0c5990b0b61c2f9494400416cc
Buomkuoth Yian <buom.nyajal@gmail.com>
2023-07-16 19:20:30 +0000

    insert into corrections

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -18,11 +18,6 @@ BEGIN
     -- Insert the correction
     INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
 
-    -- Update the average score for the user
-    UPDATE users SET average_score = (
-        SELECT AVG(score) FROM corrections WHERE user_id = user_id
-    ) WHERE id = user_id;
-
 END //
 
 DELIMITER ;
